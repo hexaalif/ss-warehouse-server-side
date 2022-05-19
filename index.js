686fcc1850a61a939a3816eabc7e886fcbd65ac6
hexaalif <hexaalif2020@gmail.com>
2022-05-20 04:05:02 +0600

    update item data loaded with mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s1lyg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('update item connected')
+  // perform actions on the collection object
+  client.close();
+});
+
+
+// middleware
+app.use(cors());
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('node ok')
