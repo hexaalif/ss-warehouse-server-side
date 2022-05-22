d56e0cc17963f4a6a782fcdd9e524f57c56ab8e2
hexaalif <hexaalif2020@gmail.com>
2022-05-22 10:48:51 +0600

    update route to singe update route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,12 +8,32 @@ const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s1lyg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('update item connected')
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        await client.connect();
+        const updateItemCollection = client.db('ss-warehouse').collection('updateProducts')
+        
+        app.get('/update', async(req, res) =>{
+            const query = {}
+            const cursor = updateItemCollection.find(query)
+            const update = await cursor.toArray()
+            res.send(update)
+        })
+
+        app.get('/update/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const update = await updateItemCollection.findOne(query)
+            res.send(update)
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
 
 
 // middleware
