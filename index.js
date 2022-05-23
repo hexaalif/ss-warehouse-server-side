9882fb0faaf6e8f0d84b9adb4d83978a99961b80
hexaalif <hexaalif2020@gmail.com>
2022-05-24 00:16:29 +0600

    update item detail

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s1lyg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -14,31 +17,53 @@ async function run(){
         await client.connect();
         const updateItemCollection = client.db('ss-warehouse').collection('updateProducts')
         
+        // add inventory
+        app.get('/update/:updateId', async(req, res) =>{
+            const id = req.params.updateId;
+            const query = {_id: ObjectId(id)}
+            const update = await updateItemCollection.findOne(query)
+            res.send(update)
+        })
         app.get('/update', async(req, res) =>{
             const query = {}
             const cursor = updateItemCollection.find(query)
             const update = await cursor.toArray()
             res.send(update)
         })
-
-        app.get('/update/:id', async(req, res) =>{
+        
+        // update item detail
+        app.put("/update/:id", async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)}
-            const update = await updateItemCollection.findOne(query)
-            res.send(update)
-        })
+            const updateItem = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+            $set: {
+                name: updateItem.name,
+                suplier: updateItem.suplier,
+                price: updateItem.price,
+                quantity: updateItem.quantity,
+                description: updateItem.description,
+                image: updateItem.image,
+            },
+            };
+            const result = await updateItemCollection.updateOne(
+            filter,
+            updateDoc,
+            options
+            );
+            res.send(result);
+        });
     }
     finally{
-
+        // client close;
     }
 }
 
 run().catch(console.dir)
 
 
-// middleware
-app.use(cors());
-app.use(express.json())
+
 
 app.get('/', (req, res) => {
     res.send('node ok')
